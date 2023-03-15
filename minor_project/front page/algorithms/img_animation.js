baa5b719c9d5f052422edd47302d0ff9266559c6
shresthasumesh13@gmail.com <shresthasumesh13@gmail.com>
2023-03-16 00:39:57 +0545

    fixed paths and added new images and improved UI

--- minor_project/front page/algorithms/img_animation.js
+++ minor_project/front page/algorithms/img_animation.js
@@ -11,29 +11,7 @@ for(let i = 0; i < img.length; i++) {
     console.log(img_ct[i]); // This value denotes how many images an algorithm specific card has
     ct.fill(0); // Initializes every cards with img0.png images
     img[i].addEventListener("mouseover", () => {
-        // console.log("I am function number" + i);
-        // console.log(img[i].src.slice(-5,-4));
-        
-        // let src = img[i].src.toString();
-        
-        // for(let x = 0;  1; x++){
-            //     if(x = 2){
-                //         img[i].src = src.replace(x, '0');
-                //     }else{
-                    //         img[i].src = src.replace(x, x + 1);
-                    //     }
-                    //     src = img[i].src;
-                    //     setTimeout(() => {
-                
-                        //     }, 200);
-                        //     img[i].addEventListener("mouseleave", () => {
-                            //         break;
-                            //     });
-        // }
-        
-        // img[i].src = "../assets/ChooseAlgo/stack/img1.png";
-        
-        // let src = img[i].src.toString();
+        img[i].src = img[i].src.replace(/img\d/, 'img0');
         ct[i] = 0;
         refreshId = setInterval(() => {
             if(ct[i] == img_ct[i]-1){
@@ -48,7 +26,7 @@ for(let i = 0; i < img.length; i++) {
         }, 450);   
     }); 
     img[i].addEventListener("mouseleave", () => {
-        img[i].src = img[i].src.replace(/img\d/, `img0`);
+        img[i].src = img[i].src.replace(/img\d/, `img${img_ct[i] - 1}`);   
         console.log(img[i].src);
         clearInterval(refreshId);
         ct[i] = 0;
